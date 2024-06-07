d26cca85e5ff999aa975ee19d3e59165ba4701af
Pavlo <108618274+PHlushkov@users.noreply.github.com>
2024-06-07 11:45:45 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: 'AIzaSyAXK_0XwDUiJq1twqMZYZjmNUKwY0BCEAY',
+  authDomain: 'time-tracking-app-3618a.firebaseapp.com',
+  projectId: 'time-tracking-app-3618a',
+  storageBucket: 'time-tracking-app-3618a.appspot.com',
+  messagingSenderId: '486389165557',
+  appId: '1:486389165557:web:ace232140eef31db9b84bc',
+  measurementId: 'G-24KP9385SR',
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
